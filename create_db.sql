7c06df0746f13e9e11b9f4a35bd695f5e96d787b
bpatty95 <pb15724@my.bristol.ac.uk>
2018-05-24 15:19:43 +0100

    minor change

--- create_db.sql
+++ create_db.sql
@@ -5,16 +5,5 @@ CREATE TABLE IF NOT EXISTS "users" (
     "salt" TEXT NOT NULL, -- salt that is appended to the password before it is hashed
     "full_name" TEXT,
     "date_of_birth" CURRENT_DATE,
-    "profile_pic_path" TEXT
 );
 
-CREATE TABLE IF NOT EXISTS "members" (
-    "user_id" INTEGER,
-    "group_id" INTEGER,
-    "is_admin" INTEGER(1),
-    "role" TEXT,
-    "start_date" DATE,
-    "end_date" DATE,
-    FOREIGN KEY ("user_id") REFERENCES "users" (user_id),
-    FOREIGN KEY ("group_id") REFERENCES "groups" (group_id)
-);
